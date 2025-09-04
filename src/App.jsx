6b547f04df82c71113f6dca6d7cf077305eeb019
Julia Kondratsiuk <kondrlias@gmail.com>
2025-09-04 17:09:47 +0300

    feat: add solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
+import FuncComponent from './Components/FuncLifeCycleComponent';
 import LifecycleComponent from './Components/LifecycleComponent';
 
 function App() {
   return (
     <>
       <LifecycleComponent />
+      <FuncComponent />
     </>
   );
 }
